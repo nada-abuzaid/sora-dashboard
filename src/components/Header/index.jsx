c3b922b7c1ec2206557f0e583de59bc98815ee53
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-06-22 10:07:44 +0300

    Create Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from '../../assets/svgs/Logo.svg';
+import styled from 'styled-components';
+
+export default function Header() {
+  return (
+    <Div>
+      <img className='logo-img' src={Logo} alt='Logo Img' />
+    </Div>
+  );
+}
+
+const Div = styled.div`
+  height: 3.4rem;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 4px 8px 0 rgba(224, 224, 224, 0.2),
+    0 6px 20px 0 rgba(201, 201, 201, 0.19);
+  .logo-img {
+    width: 6rem;
+    margin-left: 1rem;
+  }
+`;
---
+++ src/constants/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  primary: '#73314F',
+  secondary: '#ccddde',
+  black: '#000',
+  white: '#fff',
+  accordion: '#F6F6F6',
+  lightGray: '#DFDFDF',
+  midGray: '#C4C4C4',
+  checkoutGray: '#F5F4F4',
+  error: '#FF3A44',
+  transparent: 'transparent',
+};
+
+export default colors;
