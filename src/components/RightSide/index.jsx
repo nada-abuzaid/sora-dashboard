d9aed239436894461b256673cb0a9d999ed41f3b
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-06-23 11:41:48 +0300

    Finish rightSide style

--- src/components/RightSide/index.jsx
+++ src/components/RightSide/index.jsx
@@ -6,79 +6,80 @@ import colors from '../../constants/colors';
 const RightSide = () => {
   return (
     <Wrapper>
-      {/* <StaticImage
-        src='../../assets/images/auth-background.png'
+      <StaticImage
+        src='../../assets/svgs/Frame.svg'
         formats={['auto', 'webp', 'avif']}
         alt='Sora App!'
-      /> */}
+        className='background'
+      />
+      <StaticImage
+        src='../../assets/svgs/Arrow.svg'
+        formats={['auto', 'webp', 'avif']}
+        alt='Sora App!'
+        className='arrow'
+        style={{
+          position: 'absolute',
+          left: '26rem',
+          top: '2rem',
+          width: '5rem',
+        }}
+      />
       <StaticImage
         src='../../assets/svgs/Mobile.svg'
         formats={['auto', 'webp', 'avif']}
         style={{
           position: 'absolute',
           flex: 1,
-          left: '13rem',
+          left: '15rem',
+          bottom: '4rem',
           zIndex: 2,
         }}
-        className='animatedImage'
+        className='mobile'
         alt='Sora App!'
       />
-      <AnimatedShadow className='shadow' />
+      <Shadow className='shadow' />
     </Wrapper>
   );
 };
 
-const AnimatedShadow = styled.div`
+const Shadow = styled.div`
   background: ${colors.liteGreen};
   opacity: 0.5;
   position: absolute;
-  bottom: 50px;
-  filter: blur(2px);
-  left: 30px;
-  z-index: 1;
+  bottom: 45px;
+  left: 2rem;
   right: 0;
+  filter: blur(2px);
+  z-index: 1;
   margin: auto;
   height: 2rem;
   border-radius: 75%;
-  @keyframes ShadowAnimation {
-    0%,
-    100% {
-      width: 300px;
-      border-radius: 75%;
-    }
-    50% {
-      width: 100px;
-      border-radius: 40%;
-    }
-  }
-  animation: ShadowAnimation 3s ease-in-out infinite;
+  width: 300px;
+  border-radius: 75%;
 `;
 
 const Wrapper = styled.div`
-  width: 50%;
   flex: 1;
-  background: ${colors.secondary};
   height: calc(100vh - 3.4rem);
   display: flex;
   position: relative;
   align-items: flex-start;
   justify-content: center;
+  overflow: hidden;
 
-  .gatsby-image-wrapper img {
-    width: 75%;
+  .gatsby-image-wrapper.background {
+    width: 100%;
+    height: 100vh;
+    object-fit: contain;
   }
-  @keyframes MoveUpDown {
-    0%,
-    100% {
-      bottom: 0;
-    }
-    50% {
-      bottom: 20px;
-    }
+
+  .gatsby-image-wrapper.arrow {
+    height: 3rem;
   }
-  .animatedImage {
-    animation: MoveUpDown 3s ease-in-out infinite;
-    transform: scale(0.8);
+
+  .gatsby-image-wrapper.mobile {
+    width: 30%;
+    height: 73%;
   }
 `;
 
