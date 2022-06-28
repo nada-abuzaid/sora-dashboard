81c32cae863ac6397e1af4e6702dba3ceb2f8721
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-06-28 16:32:01 +0300

    Create welcome popup modal

--- src/pages-components/index.js
+++ src/pages-components/index.js
@@ -2,5 +2,6 @@ import AuthForm from './auth/AuthForm';
 import AuthRightPanel from './auth/AuthRightPanel';
 import LeftSide from './LeftSide';
 import RightSide from './RightSide';
+import WelcomeCarousel from './WelcomeCarousel';
 
-export { LeftSide, RightSide, AuthForm, AuthRightPanel };
+export { LeftSide, RightSide, AuthForm, AuthRightPanel, WelcomeCarousel };
