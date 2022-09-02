7c47f30aa342395628a18db4b809e0144bd9a064
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-09-02 10:24:58 +0300

    Deploy

--- src/components/Auth/RightPanel.jsx
+++ src/components/Auth/RightPanel.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SoraImg from '../../assets/svgs/Frame.svg';
 import Arrow from '../../assets/svgs/Arrow.svg';
+import Mobile from '../../assets/svgs/Mobile.svg';
 import { Shadow, Wrapper } from './styles';
 
 export default function MobileScreen() {
   return (
     <Wrapper>
       <img src={SoraImg} alt="Sora App!" className="background" />
+      <img src={Mobile} alt="Sora App!" className="mobile" />
       <img src={Arrow} alt="Sora App!" className="arrow" />
       <Shadow className="shadow" />
     </Wrapper>
